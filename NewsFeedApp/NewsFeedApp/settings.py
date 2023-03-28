f77e9e926904ac88d77c424edebbfd41df5ab8ed
Kelvin <54338129+Breathing-ls-Fun@users.noreply.github.com>
2023-03-28 14:58:48 -0400

    Configured Vercel Settings

--- NewsFeedApp/NewsFeedApp/settings.py
+++ NewsFeedApp/NewsFeedApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_m5qqrmw*412t^s+03blp+szxor=i+@ur%5co8vhmssn#k&l3w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
