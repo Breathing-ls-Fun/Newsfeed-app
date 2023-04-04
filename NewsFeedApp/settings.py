a871b4bc75dfeb22b2402ef8b70e1c8006578522
Kelvin <54338129+Breathing-ls-Fun@users.noreply.github.com>
2023-04-04 15:51:20 -0400

    updated db settings

--- NewsFeedApp/settings.py
+++ NewsFeedApp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'NewsFeedApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newsfeed',
+        'USER': 'admin',
+        'PASSWORD': 'nfadmin123',
+        'HOST': 'newsfeed-app.crjwqpt0y7tj.us-east-2.rds.amazonaws.com',
+        'PORT': 3306
     }
 }
 
