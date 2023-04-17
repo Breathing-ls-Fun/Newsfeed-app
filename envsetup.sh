3e6c4cbb6080095b95240b836e46a391551d360c
Kelvin <54338129+Breathing-ls-Fun@users.noreply.github.com>
2023-04-17 13:08:55 -0400

    setup jenkins files

---
+++ envsetup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -d "env" ] 
+then
+    echo "Python virtual environment exists." 
+else
+    python3 -m venv env
+fi
+
+echo $PWD
+source env/bin/activate
+
+
+pip3 install -r requirements.txt
+
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+echo "Collecting static files" 
+python3 manage.py collectstatic
+
+
+sudo chmod -R 777 logs
+echo "envsetup finished"
