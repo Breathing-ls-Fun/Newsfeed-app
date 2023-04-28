bfe1eb2883b82437d60a53f6c57fb46c15ea65f0
erchardsamoh <erchardsolodesign@gmail.com>
2023-04-27 22:27:55 -0400

    added view function for registration

--- NewsFeedApp/views.py
+++ NewsFeedApp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 def home(request):
     if not request.user.is_authenticated:
         return render(request, 'home.html', {})
 
     return redirect('/user/dashboard')
+
+def registration(request):
+    return render(request, 'registration.html', {'title':'Sign Up'})
