e2d2a8c458cd198f7a77c12d6ea16b37eba0ba5c
erchardsamoh <erchardsolodesign@gmail.com>
2023-04-27 22:30:20 -0400

    removing forms import

--- NewsFeedApp/views.py
+++ NewsFeedApp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
 
 def home(request):
     if not request.user.is_authenticated:
