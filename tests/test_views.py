34de477844cee0a15d723f8eb0a44253c4ee49ae
erchardsamoh <erchardsolodesign@gmail.com>
2023-05-05 23:56:02 -0400

    created unit tests for more templates and buttons

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,16 +3,33 @@ from django.urls import reverse
 
 class TestViews(TestCase):
     
-    def testHome(self):
+    def test_home(self):
         client = Client()
         response = client.get(reverse('home'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
-    def testSignUp(self):
+    def test_sign_up(self):
         client = Client()
         response = client.get(reverse('registration'))
-        self.assertEquals(response.status_code, 200)
-
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration.html')
+
+    def test_login(self):
+        client = Client()
+        response = client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/login.html')
+
+    def test_preferences(self):
+        client = Client()
+        response = client.get(reverse('preferences'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/preferences.html')
+
+    def test_query(self):
+        client = Client()
+        response = client.get(reverse('query'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/query.html')
 
