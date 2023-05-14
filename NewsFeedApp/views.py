fe9140ac37a6c539ab0e6883705db9638357fa66
erchardsamoh <erchardsolodesign@gmail.com>
2023-05-13 22:44:30 -0400

    added code to fetch data from weatherman class

--- NewsFeedApp/views.py
+++ NewsFeedApp/views.py
@@ -2,15 +2,36 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib import messages
-from newsapi import NewsApiClient
 from newsfeed.reporter.reporter import Reporter
+from newsfeed.reporter.weatherman import Weatherman
 
 def home(request):
-    api_key = 'fcdda744f1d94e9199337e419a17be4a'
-    reporter = Reporter(api_key)
+    news_api_key = 'd1232f6fd9ad400da947bf5a59963e10'
+    weather_api_key = '33acb338cf186037e1f0801d8945e21b'
+
+    reporter = Reporter(news_api_key)
+    weather = Weatherman(weather_api_key, location = 'New York')
+
     topnews = reporter.get_top_headlines()
+    forecast = weather.get_current_weather()
 
     print (topnews)
+    print (forecast)
+
+    hi = round(forecast['hi'])
+    lo = round(forecast['lo'])
+    current_temp = round(forecast['temp'])
+    status = forecast['status']
+    icon = forecast['icon_url']
+
+    weather_data = {
+        "hi": hi,
+        "lo": lo,
+        "current": current_temp,
+        "status": status,
+        "icon": icon
+    }
+
     latest = topnews['articles']
     title = []
     desc = []
@@ -32,7 +53,8 @@ def home(request):
     all_news = zip(title, desc, url, author, date, image)
 
     context = {
-        'all_news': all_news
+        'weather_data': weather_data,
+        'all_news': all_news,
     }
     
     return render(request, 'home.html', context)
