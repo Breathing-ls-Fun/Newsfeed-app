b47e040be9ddb92d0ad09fad483dc17c16381f1a
Stefan Tan <stefantan39@yahoo.com>
2023-05-13 20:13:20 -0400

    added categories

--- NewsFeedApp/urls.py
+++ NewsFeedApp/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('user/', include('user.urls')),
+    path('category/<str:category>', views.category, name='category')
 ]
 
     
